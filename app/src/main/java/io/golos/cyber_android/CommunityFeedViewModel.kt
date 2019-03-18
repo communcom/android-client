2c755efad27903fd3cb361d8d5d67d0a279e6869
yuri <yurivladdurain@gmail.com>
2019-03-18 18:18:14 +0300

    - merged commits related to implementation of data and domain layers

--- app/src/main/java/io/golos/cyber_android/CommunityFeedViewModel.kt
+++ app/src/main/java/io/golos/cyber_android/CommunityFeedViewModel.kt
@@ -2,6 +2,7 @@ package io.golos.cyber_android
 
 import androidx.lifecycle.ViewModel
 import io.golos.domain.interactors.CommunityFeedUseCase
+import io.golos.domain.interactors.model.UpdateOption
 
 /**
  * Created by yuri yurivladdurain@gmail.com on 2019-03-18.
@@ -9,6 +10,7 @@ import io.golos.domain.interactors.CommunityFeedUseCase
 class CommunityFeedViewModel(private val communityFeedUserCase: CommunityFeedUseCase) : ViewModel() {
     init {
         communityFeedUserCase.subscribe()
+        communityFeedUserCase.requestFeedUpdate(20, UpdateOption.REFRESH_FROM_BEGINNING)
     }
 
     override fun onCleared() {
