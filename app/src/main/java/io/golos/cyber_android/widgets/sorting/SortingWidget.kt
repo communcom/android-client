a25924777fb9c33e0f93b8110dea1c5b71f41c7a
leonidstromov <stromovleonid@gmail.com>
2019-03-19 12:33:46 +0700

    Sorting Widget

---
+++ app/src/main/java/io/golos/cyber_android/widgets/sorting/SortingWidget.kt
@@ -0,0 +1,46 @@
+package io.golos.cyber_android.widgets.sorting
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import io.golos.cyber_android.R
+import kotlinx.android.synthetic.main.view_sorting_widget.view.*
+
+/**
+ * Custom view which represents Sorting Widget. With this widget user can change the way of sorting and filtering the feed -
+ * sorting by new/top posts and filtering by time. Those sorts/filters represented by [TrendingSort] and [TimeFilter] enums.
+ */
+internal class SortingWidget : LinearLayout {
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        inflate(context, R.layout.view_sorting_widget, this)
+
+        sorting.setOnClickListener {
+
+        }
+
+        timeFiltering.setOnClickListener {
+
+        }
+    }
+
+    fun setTrendingSort(sort: TrendingSort) {
+        sorting.setText(sort.title)
+    }
+
+    fun setTimeFilter(filter: TimeFilter) {
+        timeFiltering.setText(filter.title)
+    }
+}
