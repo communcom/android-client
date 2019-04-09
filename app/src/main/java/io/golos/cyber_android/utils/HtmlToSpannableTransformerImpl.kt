9c4bbaaa1746d494a7ddb5084077f7108b800c58
yuri <yurivladdurain@gmail.com>
2019-04-09 12:11:00 +0300

    - cleaning html after transforming frpm spanned during creating post or comment

--- app/src/main/java/io/golos/cyber_android/utils/HtmlToSpannableTransformerImpl.kt
+++ app/src/main/java/io/golos/cyber_android/utils/HtmlToSpannableTransformerImpl.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.text.Html
 import android.text.Html.FROM_HTML_MODE_LEGACY
 import android.text.Spanned
+import androidx.annotation.VisibleForTesting
 import io.golos.domain.FromSpannedToHtmlTransformer
 import io.golos.domain.HtmlToSpannableTransformer
 
@@ -19,9 +20,28 @@ class HtmlToSpannableTransformerImpl : HtmlToSpannableTransformer {
 }
 
 class FromSpannedToHtmlTransformerImpl : FromSpannedToHtmlTransformer {
+    private val firstParagraphRegexp = "^<p[\\w\\s=\\\\\"]*>".toRegex()
+    private val emptyParagraphs = "<p>[\\s]*</p>".toRegex()
+    private val endingParagrapg = "<[/]?p>\$".toRegex()
+
     override fun transform(spanned: CharSequence): String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+
+        return (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
             Html.toHtml(spanned as Spanned, FROM_HTML_MODE_LEGACY)
-        else Html.toHtml(spanned as Spanned)
+        else Html.toHtml(spanned as Spanned)).let {
+            cleanHtml(it)
+        }
+    }
+
+    @VisibleForTesting
+    fun cleanHtml(htmlIn: String): String {
+        println("in html = $htmlIn")
+        return htmlIn
+            .removePrefix("\\n")
+            .removeSuffix("\\n")
+            .trim()
+            .replace(firstParagraphRegexp, "")
+            .replace(emptyParagraphs, "")
+            .replace(endingParagrapg, "")
     }
 }
