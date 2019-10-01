5035efced3246836ed59ed0eadf08e44779b6043
Alex Shevelev <al.e.shevelev@gmail.com>
2019-10-01 18:54:07 +0300

    [new_editor] Old Html-renderer has been removed.

--- posts_parsing_rendering/src/main/java/io/golos/posts_parsing_rendering/json_to_html/renderers_factory/RenderersFactory.kt
+++
@@ -1,9 +0,0 @@
-package io.golos.posts_parsing_rendering.json_to_html.renderers_factory
-
-import io.golos.posts_parsing_rendering.json_to_html.renderers.RendererBase
-import org.json.JSONObject
-
-@Suppress("SpellCheckingInspection")
-interface RenderersFactory {
-    fun getRenderer(block: JSONObject): RendererBase
-}
