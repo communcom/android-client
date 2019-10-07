35e417b76b833284b62692e058c4288ac9bdec25
Alex Shevelev <al.e.shevelev@gmail.com>
2019-10-07 18:31:40 +0300

    [editor] Add some changes in design of SelectCommunityDialog

---
+++ app/src/main/java/io/golos/cyber_android/ui/screens/main_activity/communities/select_community_dialog/SelectCommunityDialogList.kt
@@ -0,0 +1,67 @@
+package io.golos.cyber_android.ui.screens.main_activity.communities.select_community_dialog
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.golos.cyber_android.R
+import io.golos.cyber_android.ui.common.recycler_view.ListItem
+import io.golos.cyber_android.ui.screens.main_activity.communities.tabs.common.view.list.CommunityListAdapter
+import io.golos.cyber_android.ui.screens.main_activity.communities.tabs.common.view.list.CommunityListItemEventsProcessor
+import io.golos.cyber_android.ui.screens.main_activity.communities.tabs.common.view.list.CommunityListScrollListener
+import kotlinx.android.synthetic.main.fragment_communities_select_dialog_list.view.*
+
+@Suppress("unused")
+class SelectCommunityDialogList
+@JvmOverloads
+constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var listAdapter: CommunityListAdapter
+    private lateinit var listLayoutManager: LinearLayoutManager
+    private var scrollListener: CommunityListScrollListener? = null
+
+    /**
+     * The argument is position of a last visible item
+     */
+    private var onScrollListener: ((Int) -> Unit)? = null
+
+    init {
+        inflate(context, R.layout.fragment_communities_select_dialog_list, this)
+    }
+
+    fun updateList(data: List<ListItem>, listItemEventsProcessor: CommunityListItemEventsProcessor) {
+        if(!::listAdapter.isInitialized) {
+            listLayoutManager = LinearLayoutManager(context)
+
+            listAdapter = CommunityListAdapter(listItemEventsProcessor)
+            listAdapter.setHasStableIds(true)
+
+            communitiesList.isSaveEnabled = false
+            communitiesList.itemAnimator = null
+            communitiesList.layoutManager = listLayoutManager
+            communitiesList.adapter = listAdapter
+        }
+
+        listAdapter.update(data)
+    }
+
+    fun setScrollState(isScrollEnabled: Boolean) {
+        if(!::listAdapter.isInitialized) {
+            return
+        }
+
+        scrollListener?.let { communitiesList.removeOnScrollListener(it) }
+
+        if(isScrollEnabled) {
+            communitiesList.addOnScrollListener(CommunityListScrollListener(listLayoutManager) { onScrollListener?.invoke(it) })
+        }
+    }
+
+    fun setOnScrollListener(listener: ((Int) -> Unit)?) {
+        onScrollListener = listener
+    }
+}
