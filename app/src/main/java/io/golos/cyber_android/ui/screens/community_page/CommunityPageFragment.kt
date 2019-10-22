0ab729b2be59dc7e2681d9dfc3db7b5f695bad5a
kveretelnikov <k.veretelnikov@bslab.io>
2019-10-22 12:58:23 +0300

    #164 add styles for tablayout and logic show fragments

--- app/src/main/java/io/golos/cyber_android/ui/screens/community_page/CommunityPageFragment.kt
+++ app/src/main/java/io/golos/cyber_android/ui/screens/community_page/CommunityPageFragment.kt
@@ -1,12 +1,40 @@
 package io.golos.cyber_android.ui.screens.community_page
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
 import io.golos.cyber_android.R
 import io.golos.cyber_android.application.App
 import io.golos.cyber_android.application.dependency_injection.graph.app.ui.community_page.CommunityPageFragmentComponent
 import io.golos.cyber_android.databinding.FragmentCommunityPageBinding
 import io.golos.cyber_android.ui.common.mvvm.FragmentBaseMVVM
+import io.golos.cyber_android.ui.screens.followers.FollowersFragment
+import kotlinx.android.synthetic.main.fragment_community_page.*
 
 class CommunityPageFragment : FragmentBaseMVVM<FragmentCommunityPageBinding, CommunityPageModel, CommunityPageViewModel>() {
+
+    private var fragmentPagesList: List<Fragment> = ArrayList()
+
+    private val tabTitles by lazy {
+        requireContext().resources.getStringArray(R.array.community_page_tab_titles)
+    }
+
+    private val communityPagerTabAdapter by lazy {
+
+        object : FragmentPagerAdapter(childFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+            override fun getPageTitle(position: Int): CharSequence? {
+                return tabTitles.getOrNull(position)
+            }
+
+            override fun getItem(position: Int): Fragment = fragmentPagesList[position]
+
+            override fun getCount(): Int = fragmentPagesList.size
+
+        }
+    }
 
     override fun provideViewModelType(): Class<CommunityPageViewModel> = CommunityPageViewModel::class.java
 
@@ -20,7 +48,34 @@ class CommunityPageFragment : FragmentBaseMVVM<FragmentCommunityPageBinding, Com
         binding.viewModel = viewModel
     }
 
-    companion object{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTablayout()
+        initViewPager()
+    }
+
+    private fun initTablayout() {
+        tabLayout.apply {
+            setupWithViewPager(vpContent)
+            //setSelectedTabIndicator(R.drawable.)
+        }
+    }
+
+    private fun initViewPager() {
+        fragmentPagesList = createPageFragmentsList()
+        vpContent.adapter = communityPagerTabAdapter
+    }
+
+    private fun createPageFragmentsList(): MutableList<Fragment> {
+        val fragmentPagesList = ArrayList<Fragment>()
+        fragmentPagesList.add(FollowersFragment.newInstance())
+        fragmentPagesList.add(FollowersFragment.newInstance())
+        fragmentPagesList.add(FollowersFragment.newInstance())
+        fragmentPagesList.add(FollowersFragment.newInstance())
+        return fragmentPagesList
+    }
+
+    companion object {
 
         fun newInstance(): CommunityPageFragment = CommunityPageFragment()
     }
