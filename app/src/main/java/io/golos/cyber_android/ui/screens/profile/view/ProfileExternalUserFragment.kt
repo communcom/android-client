176407f41cbc5e90c6fe9f1b2e369bf4836137a6
Konstantin <konstantin.veretelnikov@saritasa.com>
2020-01-15 16:11:46 +0300

    #309 - fixed problem with the posts on external user profile fragment

--- app/src/main/java/io/golos/cyber_android/ui/screens/profile/view/ProfileExternalUserFragment.kt
+++ app/src/main/java/io/golos/cyber_android/ui/screens/profile/view/ProfileExternalUserFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import io.golos.cyber_android.application.App
-import io.golos.cyber_android.ui.shared.Tags
 import io.golos.cyber_android.ui.dto.FollowersFilter
 import io.golos.cyber_android.ui.dto.ProfileCommunities
 import io.golos.cyber_android.ui.screens.profile.di.ProfileExternalUserFragmentComponent
 import io.golos.cyber_android.ui.screens.profile.view.adapters.ProfilePagesExternalUserAdapter
 import io.golos.cyber_android.ui.screens.profile_communities.view.ProfileCommunitiesExternalUserFragment
 import io.golos.cyber_android.ui.screens.profile_followers.view.ProfileFollowersExternalUserFragment
+import io.golos.cyber_android.ui.shared.Tags
 import io.golos.domain.dto.UserDomain
 import io.golos.domain.dto.UserIdDomain
 
@@ -44,7 +44,7 @@ class ProfileExternalUserFragment : ProfileFragment() {
 
     override fun providePagesAdapter(): FragmentPagerAdapter = ProfilePagesExternalUserAdapter(
         context!!.applicationContext,
-        getDashboardFragment(this)!!.childFragmentManager,
+        childFragmentManager,
         arguments?.getParcelable(Tags.USER_ID)!!
     )
 }
