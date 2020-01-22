f93992d554fe424efef3ff36638df174be12e2fc
Konstantin <konstantin.veretelnikov@saritasa.com>
2020-01-22 06:10:55 +0300

    #296 Fix reply margin

--- app/src/main/java/io/golos/cyber_android/ui/screens/post_view/view/list/view_holders/SecondLevelCommentCollapsedViewHolder.kt
+++ app/src/main/java/io/golos/cyber_android/ui/screens/post_view/view/list/view_holders/SecondLevelCommentCollapsedViewHolder.kt
@@ -3,12 +3,10 @@ package io.golos.cyber_android.ui.screens.post_view.view.list.view_holders
 import android.content.Context
 import android.text.SpannableStringBuilder
 import android.view.ViewGroup
-import androidx.annotation.ColorInt
 import io.golos.cyber_android.R
 import io.golos.cyber_android.ui.screens.post_view.dto.post_list_items.SecondLevelCommentCollapsedListItem
 import io.golos.cyber_android.ui.screens.post_view.view_model.PostPageViewModelListEventsProcessor
 import io.golos.cyber_android.ui.shared.characters.SpecialChars
-import io.golos.cyber_android.ui.shared.extensions.getColorRes
 import io.golos.cyber_android.ui.shared.recycler_view.ViewHolderBase
 import kotlinx.android.synthetic.main.item_post_comment_second_level_collapsed.view.*
 
@@ -16,10 +14,7 @@ class SecondLevelCommentCollapsedViewHolder(
     parentView: ViewGroup
 ) : ViewHolderBase<PostPageViewModelListEventsProcessor, SecondLevelCommentCollapsedListItem>(
     parentView,
-    R.layout.item_post_comment_second_level_collapsed
-) {
-    @ColorInt
-    private val spansColor: Int = parentView.context.resources.getColorRes(R.color.default_clickable_span_color)
+    R.layout.item_post_comment_second_level_collapsed) {
 
     override fun init(listItem: SecondLevelCommentCollapsedListItem, listItemEventsProcessor: PostPageViewModelListEventsProcessor) {
         itemView.replyText.text = getReplyText(itemView.context, listItem)
