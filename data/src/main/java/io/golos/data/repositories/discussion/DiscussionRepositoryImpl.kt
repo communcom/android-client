8a5c0dcd5f638111589b3f8f7a2d5a85d5f5503a
Konstantin <konstantin.veretelnikov@saritasa.com>
2020-01-22 06:16:42 +0300

    #296 Fix comments sorting

--- data/src/main/java/io/golos/data/repositories/discussion/DiscussionRepositoryImpl.kt
+++ data/src/main/java/io/golos/data/repositories/discussion/DiscussionRepositoryImpl.kt
@@ -198,7 +198,7 @@ constructor(
         val currentUserId = currentUserRepository.userId.userId
         return apiCall {
             commun4j.getCommentsRaw(
-                sortBy = CommentsSortBy.TIME_DESC,
+                sortBy = CommentsSortBy.TIME,
                 offset = offset,
                 limit = pageSize,
                 type = commentType.mapToCommentSortType(),
