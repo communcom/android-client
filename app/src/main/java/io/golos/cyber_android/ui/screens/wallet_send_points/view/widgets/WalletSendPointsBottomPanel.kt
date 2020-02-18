8daa601e6a0492d5beeb145c90fad6df82850cc0
Alex Shevelev <al.e.shevelev@gmail.com>
2020-02-18 09:50:18 +0300

    #377 UI layout has been completed

---
+++ app/src/main/java/io/golos/cyber_android/ui/screens/wallet_send_points/view/widgets/WalletSendPointsBottomPanel.kt
@@ -0,0 +1,19 @@
+package io.golos.cyber_android.ui.screens.wallet_send_points.view.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import io.golos.cyber_android.R
+
+class WalletSendPointsBottomPanel
+@JvmOverloads
+constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.view_wallet_send_points_bottom_panel, this)
+    }
+}
