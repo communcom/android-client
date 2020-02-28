4698416b6ff4605726fbbaa234dc45802c01d6e9
Alex Shevelev <al.e.shevelev@gmail.com>
2020-02-28 11:19:08 +0300

    #454 An issue with "version" attribute has been fixed

--- domain/src/main/java/io/golos/domain/dto/block/DcoumentAttributeEntity.kt
+++ domain/src/main/java/io/golos/domain/dto/block/DcoumentAttributeEntity.kt
@@ -3,6 +3,6 @@ package io.golos.domain.dto.block
 import com.squareup.moshi.Json
 
 data class DocumentAttributeEntity(
-    @Json(name = "version") val id: String? = null,
+    @Json(name = "version") val version: String? = null,
     @Json(name = "type") val type: String? = null
 )
