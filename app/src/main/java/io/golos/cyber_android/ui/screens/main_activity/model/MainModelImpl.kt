8d9bd2c7c132bb04c7ad13bebe53c3297db2681c
Alex Shevelev <al.e.shevelev@gmail.com>
2020-03-11 08:49:21 +0300

    #466 The task has been completed

--- app/src/main/java/io/golos/cyber_android/ui/screens/main_activity/model/MainModelImpl.kt
+++ app/src/main/java/io/golos/cyber_android/ui/screens/main_activity/model/MainModelImpl.kt
@@ -1,11 +1,18 @@
 package io.golos.cyber_android.ui.screens.main_activity.model
 
 import io.golos.cyber_android.ui.shared.mvvm.model.ModelBaseImpl
+import io.golos.data.repositories.settings.SettingsRepository
 import io.golos.domain.repositories.NotificationsRepository
 import io.golos.domain.repositories.UsersRepository
 import javax.inject.Inject
 
-class MainModelImpl @Inject constructor(private val usersRepository: UsersRepository, private val notificationsRepository: NotificationsRepository): MainModel, ModelBaseImpl() {
+class MainModelImpl
+@Inject
+constructor(
+    private val usersRepository: UsersRepository,
+    private val notificationsRepository: NotificationsRepository,
+    private val settingsRepository: SettingsRepository
+): MainModel, ModelBaseImpl() {
 
     override suspend fun unsubscribeOnNotificationsChanges() {
         notificationsRepository.unsubscribeOnNotificationsChanges()
@@ -15,5 +22,6 @@ class MainModelImpl @Inject constructor(private val usersRepository: UsersReposi
         notificationsRepository.subscribeOnNotificationsChanges()
     }
 
-    override suspend fun isNeedShowFtueBoard(): Boolean = usersRepository.isNeedShowFtueBoard()
+    override suspend fun isNeedShowFtueBoard(): Boolean =
+        usersRepository.isNeedShowFtueBoard() && settingsRepository.getConfig().ftueCommunityBonus > 0
 }
