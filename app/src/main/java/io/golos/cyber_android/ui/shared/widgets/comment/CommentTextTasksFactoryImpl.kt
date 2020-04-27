f837c9ba75f4bb473125314a82993fbbd9d0a2c9
Alex Shevelev <al.e.shevelev@gmail.com>
2020-04-27 15:32:54 +0300

    #640 Links in comment are supported now

--- app/src/main/java/io/golos/cyber_android/ui/shared/widgets/comment/CommentTextTasksFactoryImpl.kt
+++ app/src/main/java/io/golos/cyber_android/ui/shared/widgets/comment/CommentTextTasksFactoryImpl.kt
@@ -1,9 +1,10 @@
 package io.golos.cyber_android.ui.shared.widgets.comment
 
+import io.golos.posts_editor.components.input.text_tasks.LinksTask
 import io.golos.posts_editor.components.input.text_tasks.MentionsTask
 import io.golos.posts_editor.components.input.text_tasks.TextTask
 import io.golos.posts_editor.components.input.text_tasks_runner.TextTasksFactory
 
 class CommentTextTasksFactoryImpl : TextTasksFactory {
-    override fun createTasks(): List<TextTask> = listOf(MentionsTask())
+    override fun createTasks(): List<TextTask> = listOf(MentionsTask(), LinksTask())
 }
