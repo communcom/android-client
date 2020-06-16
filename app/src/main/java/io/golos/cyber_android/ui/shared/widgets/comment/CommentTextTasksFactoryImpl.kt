8a9dc2795f14ca038f2b9bf432a668b6198c9626
Alex Shevelev <al.e.shevelev@gmail.com>
2020-06-16 17:00:50 +0300

    #622 Tag support has been added to comments

--- app/src/main/java/io/golos/cyber_android/ui/shared/widgets/comment/CommentTextTasksFactoryImpl.kt
+++ app/src/main/java/io/golos/cyber_android/ui/shared/widgets/comment/CommentTextTasksFactoryImpl.kt
@@ -2,9 +2,10 @@ package io.golos.cyber_android.ui.shared.widgets.comment
 
 import io.golos.posts_editor.components.input.text_tasks.LinksTask
 import io.golos.posts_editor.components.input.text_tasks.MentionsTask
+import io.golos.posts_editor.components.input.text_tasks.TagsTask
 import io.golos.posts_editor.components.input.text_tasks.TextTask
 import io.golos.posts_editor.components.input.text_tasks_runner.TextTasksFactory
 
 class CommentTextTasksFactoryImpl : TextTasksFactory {
-    override fun createTasks(): List<TextTask> = listOf(MentionsTask(), LinksTask())
+    override fun createTasks(): List<TextTask> = listOf(MentionsTask(), LinksTask(), TagsTask())
 }
