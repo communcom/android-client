23f3927419b4c45fa6bfbaa3439436a3d34b73ac
Artur <artur.karapetyan@rocketmail.com>
2020-07-17 03:04:12 +0400

    #Task688 Add sharing functionality from other apps

--- app/src/main/java/io/golos/cyber_android/ui/screens/post_edit/activity/EditorPageActivity.kt
+++ app/src/main/java/io/golos/cyber_android/ui/screens/post_edit/activity/EditorPageActivity.kt
@@ -2,7 +2,9 @@ package io.golos.cyber_android.ui.screens.post_edit.activity
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.navigation.findNavController
 import io.golos.cyber_android.R
 import io.golos.cyber_android.application.App
@@ -26,6 +28,20 @@ class EditorPageActivity : ActivityBase() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_editor_page)
+
+        if (!intent.hasExtra(Tags.ARGS)) {
+            val args = EditorPageFragment.Args().apply {
+                if (intent?.action == Intent.ACTION_SEND) {
+                    if ("text/plain" == intent.type) {
+                        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+                    } else if (intent.type?.startsWith("image/") == true) {
+                        sharedImage = intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as Uri?
+                    }
+                }
+            }
+            intent.putExtra(Tags.ARGS, args)
+        }
+
         findNavController(R.id.editorNavHost).setGraph(R.navigation.graph_editor_page, intent.extras)
     }
 
